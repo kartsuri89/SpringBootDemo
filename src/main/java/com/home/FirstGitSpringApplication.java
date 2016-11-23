9e474536c8e44e7e8ea8993a3f5dfacac8ad9969
Suriya Narayanan <kartsuri89@gmail.com>
2016-11-24 02:01:57 +0530

    Initial Commit

---
+++ src/main/java/com/home/FirstGitSpringApplication.java
@@ -0,0 +1,12 @@
+package com.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGitSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstGitSpringApplication.class, args);
+	}
+}
