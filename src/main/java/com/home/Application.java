e1b8e0b94055ee06e8f6c0ce9e9e2d0d70e21a80
Suriya Narayanan <kartsuri89@gmail.com>
2016-11-27 00:23:52 +0530

    Working till sample json creation.

--- src/main/java/com/home/Application.java
+++ src/main/java/com/home/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirstGitSpringApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstGitSpringApplication.class, args);
+		SpringApplication.run(Application.class, args);
 		System.out.println("Im Checking SpringBoot");
 		
 	}
