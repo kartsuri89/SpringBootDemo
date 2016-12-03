28b9bb7f9c647373562520923908f8d623b12e0e
Suriya Narayanan <kartsuri89@gmail.com>
2016-12-04 01:51:24 +0530

    Added CORS annotation to controller

--- src/main/java/com/home/controller/EmployeeController.java
+++ src/main/java/com/home/controller/EmployeeController.java
@@ -4,20 +4,21 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.home.model.Employee;
 import com.home.service.UserService;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 public class EmployeeController {
 
 	@Autowired
 	private UserService userService;
-	
-	@RequestMapping(path="/",method=RequestMethod.GET,produces={MediaType.APPLICATION_JSON_VALUE})
+
+	@GetMapping(path="/",produces={MediaType.APPLICATION_JSON_VALUE})
 	public List<Employee> getAllEmployee(){
 		List<Employee> getAllEmployee = userService.listAll();
 		return getAllEmployee;
